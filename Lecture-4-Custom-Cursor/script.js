75274a0f8129f718c034e0311f9f5f49a819b514
aliabbaschadhar <work.aliabbaschadhar@gmail.com>
2025-02-08 07:15:30 +0500

    Custom cursor done

--- Lecture-4-Custom-Cursor/script.js
+++ Lecture-4-Custom-Cursor/script.js
@@ -2,6 +2,22 @@ const main = document.querySelector("#main")
 
 const cursor = document.querySelector("#cursor");
 
+const imageDiv = document.querySelector("#image");
+
+imageDiv.addEventListener("mouseenter", () => {
+    cursor.innerHTML = "View more"
+    gsap.to(cursor, {
+        scale: 2,
+    })
+})
+
+imageDiv.addEventListener("mouseleave", () => {
+    cursor.innerHTML = "";
+    gsap.to(cursor, {
+        scale: 1,
+    })
+})
+
 //Easy custom cursor
 main.addEventListener("mousemove", (dets) => {
     gsap.to(cursor, {
@@ -11,3 +27,4 @@ main.addEventListener("mousemove", (dets) => {
         ease: "back.out"
     })
 })
+
