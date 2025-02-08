4e8d0a4b8494c28bb5ad284f6886660997e6e929
aliabbaschadhar <work.aliabbaschadhar@gmail.com>
2025-02-08 08:28:37 +0500

    time line project done

---
+++ Lecture-5-Timeline-Animation/script.js
@@ -0,0 +1,34 @@
+let menu = document.querySelector("#nav i");
+let cross = document.querySelector("#full i ");
+console.log(cross)
+
+// let tl = gsap.timeline({ paused: true });
+
+let tl = gsap.timeline();
+
+tl.to("#full", {
+    right: 0,
+    duration: 0.8,
+
+})
+
+tl.from("#full h4", {
+    x: 100,
+    duration: 0.7,
+    stagger: 0.3,
+    opacity: 0,
+})
+
+tl.from("#full i", {
+    opacity: 0
+})
+
+tl.pause();
+
+menu.addEventListener("click", () => {
+    tl.play();
+})
+
+cross.addEventListener("click", () => {
+    tl.reverse();
+})
