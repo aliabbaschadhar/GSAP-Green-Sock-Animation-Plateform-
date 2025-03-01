ae2eef5689cd23535490a7049087f9ab13e97162
aliabbaschadhar <work.aliabbaschadhar@gmail.com>
2025-03-02 01:16:51 +0500

    Custom cursor practice done

--- Lecture-4-Custom-Cursor/Practice-Custom-Cursor/cursor.js
+++ Lecture-4-Custom-Cursor/Practice-Custom-Cursor/cursor.js
@@ -1,5 +1,26 @@
 let cursor = document.querySelector("#cursor");
+const main = document.querySelector("#main");
+const imageDiv = document.querySelector("#image");
 
-cursor.addEventListener("mouse", (event) => {
+imageDiv.addEventListener("mouseenter", (event) => {
+    cursor.innerHTML = "View More"
+    gsap.to(cursor, {
+        scale: 2,
+    })
+})
 
+imageDiv.addEventListener("mouseleave", (event) => {
+    cursor.innerHTML = "";
+    gsap.to(cursor, {
+        scale: 1,
+    })
+})
+
+main.addEventListener("mousemove", (event) => {
+    gsap.to(cursor, {
+        x: event.x,
+        y: event.y,
+        duration: 1,
+        ease: "back.out"
+    })
 })
