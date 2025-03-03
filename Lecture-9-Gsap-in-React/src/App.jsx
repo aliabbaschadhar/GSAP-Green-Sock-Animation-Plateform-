836e34ecc4d661842d6ddea93841d2057fd8bbfa
aliabbaschadhar <work.aliabbaschadhar@gmail.com>
2025-03-03 22:34:30 +0500

    First thing done

--- Lecture-9-Gsap-in-React/src/App.jsx
+++ Lecture-9-Gsap-in-React/src/App.jsx
@@ -1,8 +1,25 @@
+import { useGSAP } from "@gsap/react"
+import gsap from "gsap"
+
 function App() {
+  useGSAP(() => {
+    gsap.to(".bg-orange-500", {
+      x: 800,
+      y: 300,
+      delay: 1,
+      duration: 1.5,
+      opacity: 1,
+      borderRadius: "50%",
+      scale: 2,
+      yoyo: true,
+      repeat: 5,
+      ease: "power3.out",
+    })
+  }, [])
   return (
     <>
-      <div className="text-3xl bg-amber-400 border-2 border-black rounded-xl">
-        Hello World
+      <div className="w-full h-full ">
+        <div className="w-64 h-64 bg-orange-500 border border-black rounded-sm"></div>
       </div>
     </>
   )
